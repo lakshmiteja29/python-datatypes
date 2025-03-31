e089451cf7b2b49b78cdc8983e3164d98c842934
lakshmiteja29 <lakshmitejathota@gmail.com>
2025-03-31 21:27:48 +0530

    Added datatyped.py file

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+# Python Basics Demonstration
+
+# Declaring variables of different data types
+string_var = "Hello, Welcome to Python!"  # String variable
+integer_var = 10  # Integer variable
+float_var = 23.7  # Float variable
+boolean_var = True  # Boolean variable
+
+# Printing the values and their types
+print("String Variable:", string_var, "Type:", type(string_var))
+print("Integer Variable:", integer_var, "Type:", type(integer_var))
+print("Float Variable:", float_var, "Type:", type(float_var))
+print("Boolean Variable:", boolean_var, "Type:", type(boolean_var))
+
+# Performing a basic arithmetic operation
+sum_result = integer_var + float_var  # Adding an integer and a float
+print("Sum of integer and float:", sum_result)
+
+# Demonstrating type casting
+integer_to_string = str(integer_var)  # Converting integer to string
+print("Integer as a string:", integer_to_string, "Type:", type(integer_to_string))
